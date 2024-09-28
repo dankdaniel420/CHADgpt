b7d5db623a62bc2f0f7da2dd415bd1e7a686efd7
Daniel <ggsan.com@gmail.com>
2024-09-29 00:24:03 +0800

    ENOUGH

--- routes/digitalcol.py
+++ routes/digitalcol.py
@@ -37,6 +37,43 @@ def evaluate():
 
     logging.info("current dict: {}".format(pair_dict))
 
+    for i in range(10):
+        modulo_weight = weight % 10
+        new_pair_dict = {}
+        for pair, count in pair_dict.items():
+            new_pairs = get_pairs(pair,modulo_weight)
+            weight += count * int(new_pairs[0][1])
+            
+            for newpair in new_pairs:
+                if newpair in new_pair_dict:
+                    new_pair_dict[newpair] += count
+                else:
+                    new_pair_dict[newpair] = count
+
+        pair_dict = new_pair_dict.copy()
+        logging.info("current colony: {}".format(i))
+
+    result.append(str(weight))
+
+
+
+    colony = data[1]["colony"]
+
+    weight = 0
+
+    for num in colony:
+        weight += int(num)
+
+    pair_dict = {}
+
+    for pair in [colony[:2], colony[1:3], colony[2:]]:
+        if pair in pair_dict:
+            pair_dict[pair] += 1
+        else:
+            pair_dict[pair] = 1
+
+    logging.info("current dict: {}".format(pair_dict))
+
     for i in range(50):
         modulo_weight = weight % 10
         new_pair_dict = {}
@@ -55,39 +92,7 @@ def evaluate():
 
     result.append(str(weight))
 
-    # # reset weight, colonies
-    # weight = 0
-    # colony = [int(x) for x in data[1]["colony"]]
-
-    # colony1 = np.array(colony[:2])
-    # colony2 = np.array(colony[1:3])
-    # colony3 = np.array(colony[2:])
-    # np.savetxt("colony1.txt", colony1)
-    # np.savetxt("colony2.txt", colony2)
-    # np.savetxt("colony3.txt", colony3)
-
-    # for num in colony:
-    #     weight += num
     
-    # for i in range(50):
-    #     modulo_weight = weight % 10
-
-    #     thread1 = CustomThread.CustomThread(file="colony1.txt", weight=modulo_weight)
-    #     thread2 = CustomThread.CustomThread(file="colony2.txt", weight=modulo_weight)
-    #     thread3 = CustomThread.CustomThread(file="colony3.txt", weight=modulo_weight)
-
-    #     thread1.start()
-    #     thread2.start()
-    #     thread3.start()
-
-    #     thread1.join()
-    #     thread2.join()
-    #     thread3.join()
-
-    #     weight += thread1.value + thread2.value + thread3.value
-    #     logging.info("current colony: {}".format(i))
-
-    # result.append(str(weight))
 
     logging.info("My result :{}".format(result))
     return json.dumps(result)
