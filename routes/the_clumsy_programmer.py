ea8cef142e29d002a732387c475118908148bc46
Daniel <ggsan.com@gmail.com>
2024-09-29 05:47:41 +0800

    points

--- routes/the_clumsy_programmer.py
+++ routes/the_clumsy_programmer.py
@@ -9,20 +9,36 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
+def cmp_words(w1:str, w2:str) -> bool:
+    count = 0
+    for i in range(len(w1)):
+        if w1[i] == w2[i]:
+            count += 1
+    
+    return count == (len(w1) -1)
+
 
 @app.route('/the-clumsy-programmer', methods=['POST'])
 def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
-    correctionDict = {}
     corrections = []
     
-    dictionary = data.get("dictionary")
-    mistypes = data.get("mistypes")
+    dictionary = data[0]["dictionary"]
+    mistypes = data[0]["mistypes"]
 
- 
+    for word in mistypes:
+        dict_word = ""
+        for ans in dictionary:
+            if len(ans) != len(word):
+                continue
 
+            if cmp_words(ans, word):
+                dict_word = ans
+                corrections.append(dict_word)
+                break
         
+        dictionary.remove(dict_word)
 
-    return jsonify(result)
+    return jsonify({"corrections":corrections})
