586daf368e15326bedd94ed8e955fd841dc507cf
Daniel <ggsan.com@gmail.com>
2024-09-28 16:48:18 +0800

    try 3

--- routes/digitalcol.py
+++ routes/digitalcol.py
@@ -5,29 +5,30 @@ from flask import request
 
 from routes import app
 
-def nextgeneration(current:list, weight: int) -> int:
+def nextgeneration(current:list, weight: int) -> (list, int):
 
     last = current[-1]
 
     modulo_weight = weight % 10
 
-    j = 0
+    new_list = []
 
     for i in range(len(current)-1):
-        int_p1 = current[i + j]
-        int_p2 = current[i + 1 + j]
+        int_p1 = current[i]
 
-        signature = int_p1 - int_p2
+        signature = int_p1 - current[i + 1]
         if signature < 0:
             signature += 10
         
         int_child = (signature + modulo_weight) % 10
         weight += int_child
 
-        current.insert(i + j + 1, int_child)
-        j += 1
+        new_list.append(int_p1)
+        new_list.append(int_child)
 
-    return weight
+    new_list.append(last)
+
+    return new_list, weight
 
 
 @app.route('/digital-colony', methods=['POST'])
@@ -36,21 +37,27 @@ def evaluate():
     logging.info("data received: {}".format(data))
     weight = 0
 
-    colony = [int(x) for x in data[0]["colony"]]
     result = []
+
+    colony = [int(x) for x in data[0]["colony"]]
 
     for num in colony:
         weight += num
     
     for i in range(10):
-        weight = nextgeneration(colony, weight)
+        colony, weight = nextgeneration(colony, weight)
         logging.info("current colony: {}".format(i))
 
     result.append(str(weight))
 
-    for i in range(40):
-        weight = nextgeneration(colony, weight)
-        logging.info("current colony: {}".format(10 + i))
+    colony = [int(x) for x in data[1]["colony"]]
+
+    for num in colony:
+        weight += num
+
+    for i in range(50):
+        colony, weight = nextgeneration(colony, weight)
+        logging.info("current colony: {}".format(i))
     
     result.append(str(weight))
 
