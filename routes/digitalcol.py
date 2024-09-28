2d12ab5dad35b7fee2d91ab218401048d8d226ff
Daniel <ggsan.com@gmail.com>
2024-09-28 15:58:59 +0800

    int list

--- routes/digitalcol.py
+++ routes/digitalcol.py
@@ -5,60 +5,55 @@ from flask import request
 
 from routes import app
 
-WEIGHT = 0
+def nextgeneration(current:list, weight: int) -> (list, int):
 
-def nextgeneration(current:str) -> str:
-    global WEIGHT 
-    currList = list()
+    last = current[-1]
+    new_list = []
 
-    modulo_weight = WEIGHT % 100
+    modulo_weight = weight % 100
 
     for i in range(len(current)-1):
-        char_p1 = current[i]
-        currList.append(char_p1)
-
-        int_p1 = int(char_p1)
-        int_p2 = int(current[i+1])
+        int_p1 = current[i]
+        int_p2 = current[i+1]
 
         signature = int_p1 - int_p2
         if signature < 0:
             signature += 10
         
         int_child = (signature + modulo_weight) % 10
-        WEIGHT += int_child
+        weight += int_child
 
-        currList.append(str(int_child))
+        new_list.append(int_p1)
+        new_list.append(int_child)
 
-    currList.append(current[-1])
+    new_list.append(current[-1])
 
-    return "".join(currList)
+    return new_list, weight
 
 
 @app.route('/digital-colony', methods=['POST'])
 def evaluate():
     data = request.get_json()
     logging.info("data received: {}".format(data))
-    global WEIGHT
+    weight = 0
 
-    colony = data[0]["colony"]
-
+    colony = [int(x) for x in data[0]["colony"]]
     result = []
 
     for num in colony:
-        WEIGHT += int(num)
-        logging.info("current weight: {}".format(WEIGHT))
+        weight += num
     
     for i in range(10):
-        colony = nextgeneration(colony)
-        logging.info("current colony: {}".format(colony))
+        colony, weight = nextgeneration(colony, weight)
+        logging.info("current colony: {}".format(i))
 
-    result.append(str(WEIGHT))
+    result.append(str(weight))
 
     for i in range(40):
-        colony = nextgeneration(colony)
-        logging.info("current colony: {}".format(colony))
+        colony, weight = nextgeneration(colony, weight % 100)
+        logging.info("current colony: {}".format(10 + i))
     
-    result.append(str(WEIGHT))
+    result.append(str(weight))
 
     logging.info("My result :{}".format(result))
     return json.dumps(result)
